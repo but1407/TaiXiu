fd8c192e26f64a0a909cae8cb2321cf78c25d6ee
but1407 <dinhtuanbut1472002@gmail.com>
2024-05-06 22:39:15 +0700

    end TaiXiu

--- src/main/KhoiDau.java
+++ src/main/KhoiDau.java
@@ -1,38 +1,45 @@
 package main;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
 public class KhoiDau {
-    public static int luaChon(double taiKhoanNguoiChoi, Scanner sc){
-        System.out.println("------------------------------MỜI BẠN LỰA CHỌN------------------------------");
-        System.out.println("Chọn (1) để tiếp tục chơi.");
-        System.out.println("chọn phím bất kỳ để thoát");
-        int luaChon = sc.nextInt();
-        if(luaChon == 1){
-            System.out.println("*** Bắt đầu chơi.");
-            //Đặt cược
-            System.out.println("***** Tài khoản của bạn: " + taiKhoanNguoiChoi + ", bạn muốn cược bao nhiêu?");
-            double datCuoc = 0;
-            do{
-                System.out.println("****** Đặt cược (0 < số tiền cược <= " + taiKhoanNguoiChoi +"):");
-                datCuoc = sc.nextDouble();
-            } while(datCuoc <= 0 || datCuoc > taiKhoanNguoiChoi);
+    public static int LuaChonTaiXiu(int luaChonTaiXiu,Scanner sc) {
+        do{
+            System.out.println("******** Chọn: (1) <-> Tài || (2) <-> Xỉu");
+            luaChonTaiXiu = sc.nextInt();
+        } while(luaChonTaiXiu != 1 && luaChonTaiXiu != 2);
 
-            //Lựa chọn tài xỉu
-            int luaChonTaiXiu = 0;
-            do{
-                System.out.println("******** Chọn: (1) <-> Tài || (2) <-> Xỉu");
-                luaChonTaiXiu = sc.nextInt();
-            } while(luaChonTaiXiu != 1 && luaChonTaiXiu != 2);
-            
-            //Tung xúc sắc
-            Random xucxac1 = new Random();
-            Random xucxac2 = new Random();
-            Random xucxac3 = new Random();
+        return luaChonTaiXiu;
+    }
+    public static double VeTai(int luaChonTaiXiu, int tong, double taiKhoanNguoiChoi, double datCuoc, NumberFormat numf) {
+        System.out.println("******** Tổng là: " + tong + " => Tài");
+        if (luaChonTaiXiu == 1){
+            System.out.println("******** Bạn đã thắng cược");
+            taiKhoanNguoiChoi += datCuoc;
+            System.out.println("******** Tài khoản của bạn là: " + numf.format(taiKhoanNguoiChoi));
 
-
+        } else {
+            System.out.println("******** Bạn đã thua cược");
+            taiKhoanNguoiChoi -= datCuoc;
+            System.out.println("******** Tài khoản của bạn là: " + numf.format(taiKhoanNguoiChoi));
         }
-        return luaChon;
+        return taiKhoanNguoiChoi;
+    }
+    public static double VeXiu(int luaChonTaiXiu, int tong, double taiKhoanNguoiChoi, double datCuoc, NumberFormat numf) {
+        System.out.println("******** Tổng là: " + tong + " => Xỉu");
+        if (luaChonTaiXiu == 2) {
+            System.out.println("******** Bạn đã thắng cược");
+            taiKhoanNguoiChoi += datCuoc;
+            System.out.println("******** Tài khoản của bạn là: " + numf.format(taiKhoanNguoiChoi));
+
+        } else {
+            System.out.println("******** Bạn đã thua cược");
+            taiKhoanNguoiChoi -= datCuoc;
+            System.out.println("******** Tài khoản của bạn là: " + numf.format(taiKhoanNguoiChoi));
+        }
+        return taiKhoanNguoiChoi;
     }
 }
--- src/main/TaiXiu.java
+++ src/main/TaiXiu.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.text.NumberFormat;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -12,14 +16,68 @@ import java.util.Scanner;
 * */
 public class TaiXiu {
     public static void main(String[] args) {
-        double taikhoanNguoiChoi = 5000;
         Scanner sc = new Scanner(System.in);
-        int luaChon = 1;
-        do {
-            //Khởi đầu
-            KhoiDau khoidau = new KhoiDau();
-            luaChon = khoidau.luaChon(taikhoanNguoiChoi, sc);
-            
-        } while(luaChon!=1);
+        System.out.println("Nhập số tiền bạn đang có:");
+        try {
+            double taiKhoanNguoiChoi = sc.nextDouble();
+            int luaChon = 0;
+            do {
+                //Khởi đầu
+                KhoiDau khoidau = new KhoiDau();
+                //format VND
+                Locale lc = new Locale("vi", "VN");
+                //NumberFormat numf = NumberFormat.getInstance(lc);
+                NumberFormat numf = NumberFormat.getCurrencyInstance(lc);
+
+                System.out.println("------------------------------MỜI BẠN LỰA CHỌN------------------------------");
+                System.out.println("Chọn (1) để tiếp tục chơi.");
+                System.out.println("chọn phím bất kỳ để thoát");
+                luaChon = sc.nextInt();
+                if (luaChon == 1) {
+                    System.out.println("*** Bắt đầu chơi.");
+
+                    //Đặt cược
+                    System.out.println("***** Tài khoản của bạn: " + numf.format(taiKhoanNguoiChoi) + ", bạn muốn cược bao nhiêu?");
+                    double datCuoc = 0;
+                    do {
+                        System.out.println("****** Đặt cược (0 < số tiền cược <= " + numf.format(taiKhoanNguoiChoi) + "):");
+                        datCuoc = sc.nextDouble();
+                    } while (datCuoc <= 0 || datCuoc > taiKhoanNguoiChoi);
+
+                    //Lựa chọn tài xỉu
+                    int luaChonTaiXiu = 0;
+                    luaChonTaiXiu = khoidau.LuaChonTaiXiu(luaChonTaiXiu, sc);
+                    if(luaChonTaiXiu == 1){
+                        System.out.println("***Bạn chọn Tài");
+                    } else{
+                        System.out.println("***Bạn chọn Xỉu");
+                    }
+
+                    //Tung xúc sắc
+                    Random xucXac1 = new Random();
+                    Random xucXac2 = new Random();
+                    Random xucXac3 = new Random();
+
+                    int giaTri1 = xucXac1.nextInt(5) + 1;
+                    int giaTri2 = xucXac2.nextInt(5) + 1;
+                    int giaTri3 = xucXac3.nextInt(5) + 1;
+                    int tong = giaTri1 + giaTri2 + giaTri3;
+
+                    //Tính toán kết quả
+                    System.out.println("******** Kết quả: " + giaTri1 + " + " + giaTri2 + " + " + giaTri3);
+                    if (tong == 3 || tong == 18) {
+                        taiKhoanNguoiChoi -= datCuoc;
+                        System.out.println("******** Tổng là: " + tong + " => nhà cái ăn hết, bạn đã thua cược!");
+                        System.out.println("******** Tài khoản của bạn là: " + numf.format(taiKhoanNguoiChoi));
+                    } else if (tong >= 4 && tong <= 10) {
+                        taiKhoanNguoiChoi = khoidau.VeXiu(luaChonTaiXiu, tong, taiKhoanNguoiChoi, datCuoc, numf);
+                    } else {
+                        taiKhoanNguoiChoi = khoidau.VeTai(luaChonTaiXiu, tong, taiKhoanNguoiChoi, datCuoc, numf);
+                    }
+                }
+            } while (luaChon == 1);
+        } catch (InputMismatchException e){
+            System.out.println("bạn nhập sai kiểu dữ liệu!");
+        }
     }
 }
